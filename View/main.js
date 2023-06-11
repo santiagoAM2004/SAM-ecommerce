7bbcdda016d65c1ca32c4fd3026c50353187997b
santiagoAM2004 <sam2454221@gmail.com>
2023-06-11 02:56:14 -0500

    create the logic for hide a few elements with js

--- View/main.js
+++ View/main.js
@@ -1,8 +1,28 @@
 const menuEmail = document.querySelector('.navbar-email');
 const subDesktopMenu = document.querySelector('.sub-desktop-menu');
 
+const hamMenu = document.querySelector('.menu');
+const subMenuMobile = document.querySelector('.mobile-menu');
+
+const detailProducts = document.querySelector('.product-detail');
+const shopingCarIcon = document.querySelector('.navbar-shopping-cart');
+
 menuEmail.addEventListener('click', toggleSubDesktopMenu);
 
 function toggleSubDesktopMenu() {
     subDesktopMenu.classList.toggle('inactive');
+}
+
+hamMenu.addEventListener('click', toggleSubMobileMenu);
+
+function toggleSubMobileMenu(){
+    detailProducts.classList.add('inactive');
+    subMenuMobile.classList.toggle('inactive');
+}
+
+shopingCarIcon.addEventListener('click', toggleDetailProduct);
+
+function toggleDetailProduct(){
+    subMenuMobile.classList.add('inactive');
+    detailProducts.classList.toggle('inactive');
 }
